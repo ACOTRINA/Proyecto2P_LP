cece4b18a566c441e382b84b0afcb184f162ac66
Odalys Guzman <oyguzman@espol.edu.ec>
2021-02-02 23:33:35 -0500

    Ventana y moodelos de mascota

---
+++ adoptmelp/lib/api/apiMascota.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import '../modelos/mascota.dart';
+import 'package:http/http.dart' as http;
+
+class MascotaProvider{
+
+  final url="http://10.0.2.2:8000/api/mascotas";
+
+  Future<List<Mascota>> getMascotasAdopcion() async {
+
+    List<Mascota> mascotas = [];
+    final response = await http.get(url);
+    if(response.statusCode==200){
+      var datos=json.decode(response.body) as List;
+      mascotas = datos.map<Mascota>((json) => Mascota.fromJson(json)).toList();
+      return mascotas;
+    }
+    return mascotas;
+  }
+
+  
+}
